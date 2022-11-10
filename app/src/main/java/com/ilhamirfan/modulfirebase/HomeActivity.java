6ab0dc27c7c80be2a727966e1d13445e280c16d9
Ilham Irfan <42286970+IlhamBachtiarIrfani@users.noreply.github.com>
2022-11-10 12:37:51 +0700

    add firebase email login

---
+++ app/src/main/java/com/ilhamirfan/modulfirebase/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.ilhamirfan.modulfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private TextView userDataText;
+    private Button logoutButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        userDataText = findViewById(R.id.userDataText);
+        logoutButton = findViewById(R.id.LogoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Logout();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            userDataText.setText(currentUser.getEmail());
+        } else {
+            finish();
+        }
+    }
+
+    private void Logout() {
+        FirebaseAuth.getInstance().signOut();
+    }
+}
---
+++ app/src/main/java/com/ilhamirfan/modulfirebase/MainActivity.java
@@ -0,0 +1,80 @@
+package com.ilhamirfan.modulfirebase;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private EditText emailInput, passwordInput;
+    private Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailInput = findViewById(R.id.EmailInput);
+        passwordInput = findViewById(R.id.PasswordInput);
+        loginBtn = findViewById(R.id.LoginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                RequestSignIn(email, password);
+            }
+        });
+    }
+
+    private void RequestSignIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent homeActivity = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(homeActivity);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent homeActivity = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(homeActivity);
+        }
+    }
+}
